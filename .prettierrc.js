8aba403c957d65fda54b1d825ed90fc0e86b13e0
fish <312412223@qq.com>
2025-01-23 02:20:08 +0800

    feat: 新增代码规范化eslint+prettier+husky+commintlint配置

---
+++ .prettierrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // 箭头函数只有一个参数的时候可以省略括号
+  arrowParens: 'avoid',
+  // 括号内不要出现空格
+  bracketSpacing: false,
+  // 行结束符使用Unix格式
+  endOfLine: 'crlf',
+  // true: Put > on the last line instead of at a new line
+  jsxBracketSameLine: false,
+  // 换行方式
+  quoteProps: 'preserve',
+  // 行尾逗号
+  trailingComma: 'es5',
+  // 使用单引号
+  singleQuote: true,
+  // 行宽
+  printWidth: 100,
+  // 分号
+  semi: true,
+  // 缩进
+  tabWidth: 2,
+  // 使用tab缩进
+  useTabs: false,
+  // 对象的key仅在必要时用引号
+  quoteProps: 'as-needed',
+  // 在对象文字中打印括号之间的空格
+  bracketSpacing: true,
+  // 解析器
+  parser: 'typescript',
+};
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals.ts';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
